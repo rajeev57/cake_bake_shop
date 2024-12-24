2ec0a675a64e92fae45533aedf404850ab05999e
RAJEEV <rajeev9708037174@gmail.com>
2024-12-24 21:34:41 +0530

    update the code for ' single quotes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2024 Cake'N'Bake shop</p>
+      <p>&copy; 2024 Cake&apos;N&apos;Bake shop</p>
       <p>Phone: +91 8863007949</p> {/* Replace with actual phone number */}
       <p>Address: near bank of baroda, hathidah, patna, bihar</p> {/* Replace with actual address */}
       <p>Pin Code: 803301</p>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Welcome to the Cake'N'Bake Shop</h1>
+      <h1>Welcome to the Cake&apos;N&apos;Bake Shop</h1>
     </header>
   );
 }
